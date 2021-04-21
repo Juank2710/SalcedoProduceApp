588142aa247c89c9b1d627f85aef87f21b168c43
Juan Carlos Diaz <jc2876967@gmail.com>
2021-04-21 18:42:09 -0500

    prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,11 @@ import { ComponentsModule } from './components/components.module';
 
 //import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(firebaseConfig),AngularFirestoreModule,
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(environment.firebaseConfig),AngularFirestoreModule,
     ComponentsModule
   
   ],
